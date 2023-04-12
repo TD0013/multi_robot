74cba764770ac918f47a43b6006781173ec671d1
STRIX 20.04 <duhantanishq@gmail.com>
2023-04-12 17:20:33 +0530

    cleaning

--- build/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest
-# Build directory: /home/td0013/catkin_ws/src/multi_robot/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
